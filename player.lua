f9fdd0e43fb82f50be81c21dd859da990170bad7
Martin Felis <martin@fysx.org>
2012-01-27 21:04:24 +0100

    added player file, added .gitignore

---
+++ player.lua
@@ -0,0 +1,45 @@
+local function newHero()
+	local hero = {
+		x = 0,
+		y = 200,
+		commandHistory = {},
+		lastCommand = 1,
+		controllerState = {},
+	}
+
+	function hero:insertCommand(command, arguments, time)
+		table.insert(self.commandHistory, {command = command, arguments = arguments,time = time})
+	end
+
+	function hero:moveRightKey(state)
+		self.controllerState["right"] = state
+	end
+
+	function hero:moveLeftKey(state)
+		self.controllerState["left"] = state
+	end
+
+	function hero:executeHistory(currentRoundTime)
+		while true do		
+			local command = self.commandHistory[self.lastCommand]
+			if command and currentRoundTime >= command.time then
+				command.command(unpack(command.arguments))
+				self.lastCommand = self.lastCommand + 1
+			else
+				break
+			end
+		end
+	end
+
+	function hero:updatePosition(dt)
+		if self.controllerState["left"] then
+			self.x = self.x - dt * 50
+		end
+		if self.controllerState["right"] then
+			self.x = self.x + dt * 50
+		end
+	end
+	return hero
+end
+
+return newHero
